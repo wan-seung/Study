06b31afe92786ecafe692b0c1692bba92b6adb99
wan-seung <reddht721@gmail.com>
2021-03-29 23:36:11 +0900

    study

--- Todolist/info.js
+++ Todolist/info.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
 
 v8 > Chrome, Opera
@@ -7,3 +9,13 @@ v8 > Chrome, Opera
 엔진은 프로세스 각 단계마다 최적화를 진행합니다.
 
 */
+
+alert("i'm javascript!!");
+alert(3 + 1 + 2);
+
+let a = 3;
+let b = '이숫자는 몇일까?';
+
+console.log(b + a);
+
+// let && const => 변수와 상수
