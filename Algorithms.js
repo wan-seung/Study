f287b13cf85b563951da8578b89122bd8a8f2205
wan-seung <reddht721@gmail.com>
2021-04-10 23:05:56 +0900

    Algorithms Study

--- Algorithms.js
+++ Algorithms.js
@@ -170,3 +170,29 @@ var maximumWealth = function (accounts) {
   }
   return max;
 };
+
+// 1512. Number of Good Pairs
+
+// Given an array of integers nums.
+
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+// Return the number of good pairs.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+var numIdenticalPairs = function (nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        result++;
+      }
+    }
+  }
+  return result;
+};
