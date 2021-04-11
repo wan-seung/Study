6de9d69e0802dcf1d9ad6fe097dc1e9ca5556557
wan-seung <reddht721@gmail.com>
2021-04-11 18:15:52 +0900

    Algorithms Study

--- Algorithms.js
+++ Algorithms.js
@@ -196,3 +196,24 @@ var numIdenticalPairs = function (nums) {
   }
   return result;
 };
+
+// 1528. Shuffle String
+
+// Given a string s and an integer array indices of the same length.
+
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+// Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+var restoreString = function (s, indices) {
+  // let result = new Array(indices.length).fill("");
+  let result = [];
+  for (let i = 0; i < indices.length; i++) {
+    result[indices[i]] = s[i];
+  }
+  return result.join('');
+};
