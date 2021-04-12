8f87496d1a8109988e1de51f58ea886308f94ffc
wan-seung <reddht721@gmail.com>
2021-04-12 23:33:52 +0900

    Algorithms Study

--- Algorithms.js
+++ Algorithms.js
@@ -217,3 +217,26 @@ var restoreString = function (s, indices) {
   }
   return result.join('');
 };
+
+// 771. Jewels and Stones
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+var numJewelsInStones = function (J, S) {
+  let result = 0;
+  for (let i = 0; i < J.length; i++) {
+    for (let j = 0; j < S.length; j++) {
+      if (J[i] === S[j]) {
+        result++;
+      }
+    }
+  }
+  return result;
+};
