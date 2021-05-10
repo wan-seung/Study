f936161891421f7e1dbf81b0ebd12a39b407f8e5
wan-seung <reddht721@gmail.com>
2021-05-10 18:01:35 +0900

    corejs Study

--- core.js
+++ core.js
@@ -89,3 +89,46 @@ function d() {
   console.log(b); // (3)
 }
 d();
+
+//예제 2-8 함수를 정의하는 세 가지 방식
+function a() {
+  return 'hello';
+} // 함수 선언문. 함수명 a가 곧 변수명.
+a(); // 실행 ok
+
+var b = function () {}; // (익명) 함수 표현식. 변수명 b가 곧 함수명
+b(); // 실행 ok
+
+var c = function d() {}; // 기명 함수 표현식. 변수명은 c, 함수명은 d.
+c(); // 실행 ok
+// d(); // 에러!
+
+//예제 2-9 함수 선언문과 함수 표현식 (1) - 원본코드
+console.log(sum(1, 2));
+console.log(multiply(3, 4));
+
+function sum(a, b) {
+  //함수 선언문 sum
+  return a + b;
+}
+
+var multiply = function (a, b) {
+  // 함수 표현식 multiply
+  return a * b;
+};
+
+//예제 2-10 함수 선언문과 함수 표현식 (2) - 호이스팅을 마친 상태
+var sum = function sum(a, b) {
+  // 함수 선언문은 전체를 호이스팅 합니다.
+  return a + b;
+};
+
+var multiply; // 변수는 선언부만 끌어올립니다.
+
+console.log(sum(1, 2));
+console.log(multiply(3, 4));
+
+multiply = function (a, b) {
+  // 변수의 할당부는 원래 자리에 남겨둡니다.
+  return a * b;
+};
